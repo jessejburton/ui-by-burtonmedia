c6c4eb08c190ad02d1919560d5a6b022c8994baa
Jesse James Burton <jesse@burtonmediainc.com>
2020-07-14 17:37:23 -0700

    Buttons added as well as started working on Radio and Checkboxes. Selects were added as well

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,9 @@
 document.querySelectorAll("main aside > nav div a:first-child").forEach(link => {
   link.addEventListener("click", (e) => {
-    e.preventDefault()
     e.currentTarget.parentNode.classList.toggle("open")
   })
+})
+
+document.querySelector("select[name=theme]").addEventListener("change", (event) => {
+  document.querySelector("body").classList = event.target.value
 })
